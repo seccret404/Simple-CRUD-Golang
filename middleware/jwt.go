288ad3dfb4bcdbc5dc11c191efd944a2f6e4dfa7
seccret404 <edwardtua25@gmail.com>
2025-02-26 19:54:11 +0700

    Update Add middleware JWT

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,2 +1,42 @@
 package middleware
 
+import (
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateToken(userID int) (string, error) {
+	secret := os.Getenv("JWT_KEY_SECRET")
+
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp": time.Now().Add(time.Hour * 72 ).Unix(), //set batas expire
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	return token.SignedString([] byte(secret))
+
+}
+
+func JwtMiddleware(c *fiber.Ctx) error{
+	tokenString := c.Get("Authorization")
+
+	if tokenString == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error" : "Token nya belum ade bro.."})
+	}
+
+	secret := os.Getenv("JWT_KEY_SECRET")
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error){
+		return []byte(secret), nil
+	})
+
+	if err != nil || !token.Valid {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error" : "token invalid"})
+	}
+
+	return c.Next()
+
+}
